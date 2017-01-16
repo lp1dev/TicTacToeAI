1b18e73d79147708f04c06b20cde32afc55f94c6
Lupin <lp1@lp1.eu>
2017-01-16 02:37:45 +0100

    Updated training of the network and dataset

--- neural_network.py
+++ neural_network.py
@@ -11,6 +11,8 @@ class NeuralNetwork():
     def train(self):
         for game in self.dataset:
             for turn, grid in enumerate(game['grids']):
+                if game['player'] == 1:
+                    grid = grid.replace('X', 'O')
                 for i, box in enumerate(grid):
                     self.neurons[i]["items"][box] += 1
                     self.neurons[i]["weight"][box] = (self.neurons[i]["weight"][box] + game['moves'][turn + 1])
@@ -26,25 +28,34 @@ class NeuralNetwork():
     def get_next_move(self, grid):
         print("Given grid is ")
         print(grid)
+        if "0" not in grid:
+            return 0
         output = {"weight": 0, "items": 0}
+        possible_moves = []
         for i, box in enumerate(grid):
             if self.neurons[i]['items'][box] == 0:
                 print("[Warning] The neural network hasn't been trained for every input")
             else:
                 neuron_choice = self.neurons[i]['weight'][box] / self.neurons[i]['items'][box];
-                if grid[int(round(neuron_choice))] == '0':
-                    print("Neuron choice : %s" %neuron_choice)
+                if grid[round(neuron_choice)] == '0':
+                    possible_moves.append(round(neuron_choice))
+                    print("Neuron choice : %s" %round(neuron_choice))
                     output['weight'] = output['weight'] + neuron_choice
                     output['items'] += 1
-        if output['items'] != 0 and grid[int(round(output['weight'] / output['items']))] == '0':
-            output['weight'] /= output['items']
-            print("AI has chosen ")
-            print(output)
-            return int(round(output['weight']))
-
-        else:
-            print("AI Couldn't find a smart move. Retuning a random move")
-            while True:
-                move = randrange(0, self.row_size * self.row_size)
-                if grid[move] == '0':
-                    return move
+        if output['items'] != 0:
+            move = int(round(output['weight'] / output['items']))
+            if grid[move] == '0':
+                output['weight'] /= output['items']
+                print("AI has chosen ")
+                print(output)
+                return int(round(output['weight']))
+            else:
+                print("AI would have played %i but it was impossible" %move)
+        print("Trying to return another one of the possible moves")
+        if len(possible_moves):
+            return possible_moves[randrange(0, len(possible_moves))]
+        print("AI Couldn't find a smart move. Retuning a random move")
+        while True:
+            move = randrange(0, self.row_size * self.row_size)
+            if grid[move] == '0':
+                return move
